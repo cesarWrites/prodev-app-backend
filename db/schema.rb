cb7de33b18cfb95c1163785523fa353d1d7451ee
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-01 22:34:49 +0300

    add table migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_190039) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_192846) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.integer "rating"
@@ -21,8 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_190039) do
   create_table "lessons", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.integer "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
+  add_foreign_key "lessons", "courses"
 end
