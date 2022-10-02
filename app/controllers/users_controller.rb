f3d350afc730ad70333721531940b0c4c4f86157
Sarah Mukuti <cesarwrites24@gmail.com>
2022-10-02 11:33:14 +0300

    add autenntication controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   resources :courses, only: [:show, :index] do
     resources :lessons, only: [:show, :index, :create]
   end
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/users", to: "users#index"
 end
